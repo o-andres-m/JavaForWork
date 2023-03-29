c9c8a8af757466b4115479dd4b7d9d2ab9989576
o-andres-m <osvmarti@host.docker.internal>
2023-03-29 11:46:20 +0200

    ActorDto 2.0

--- demo/src/main/java/com/example/domains/entities/dtos/ActorDto.java
+++ demo/src/main/java/com/example/domains/entities/dtos/ActorDto.java
@@ -1,7 +1,26 @@
 package com.example.domains.entities.dtos;
 
+import lombok.Value;
+
+//Si le ponemos VALUE es como un "DATA" pero sin SETTERS y con un constructor completo
+//Esto es porque sera INMUTABLE, es decir, no lo podremos modificar...
+@Value
+
+/*
+ * Si le ponemos @Data, sera MUTABLE, y habria que ponerle el @AllArgsConstructor
+ * y el @NoArgsConstructor. Porque tiene por defecto solo el constructor SIN PARAMETROS
+ * Y al ponerle el @AllArgsConstructor es necesario meterle el @Noargs.. para que
+ * tenga ambos.
+ */
 public class ActorDto {
+	
+	private int actorId;
 
+	private String firstName;
 
+	private String lastName;
+
+	private int edad;
+	
 
 }
