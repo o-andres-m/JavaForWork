08ef968bab71aea4ed03098949b6ab321437b2f5
o-andres-m <elhinchon@gmail.com>
2023-04-03 13:12:07 +0200

    Tasklet

--- lotes-batch/src/main/java/com/example/batch/PersonasBatchConfiguration.java
+++ lotes-batch/src/main/java/com/example/batch/PersonasBatchConfiguration.java
@@ -121,14 +121,41 @@ public class PersonasBatchConfiguration {
 	
 	// JOB: From DATABASE to CSV
 	
+//	@Bean
+//	public Job personasJob(PersonasJobListener listener, Step importCSV2DBStep1,
+//							Step exportDB2CSVStep) {
+//		return new JobBuilder("personasJob", jobRepository)
+//			.incrementer(new RunIdIncrementer())
+//			.listener(listener)
+//			.start(importCSV2DBStep1)
+//			.next(exportDB2CSVStep)
+//			.build();
+//	}
+	
+	
+	// TASKLET
 	@Bean
-	public Job personasJob(PersonasJobListener listener, Step importCSV2DBStep1,
-							Step exportDB2CSVStep) {
+	public FTPLoadTasklet ftpLoadTasklet(@Value("${input.dir.name:./ftp}") String dir) {
+		FTPLoadTasklet tasklet = new FTPLoadTasklet();
+		tasklet.setDirectoryResource(new FileSystemResource(dir));
+		return tasklet;
+	}
+	
+	@Bean
+	public Step copyFilesInDir(FTPLoadTasklet ftpLoadTasklet) {
+		return new StepBuilder("copyFilesInDir", jobRepository)
+			.tasklet(ftpLoadTasklet, transactionManager)
+			.build();
+	}
+	
+	@Bean
+	public Job personasJob(PersonasJobListener listener,
+			Step importCSV2DBStep1, Step exportDB2CSVStep, Step copyFilesInDir) {
 		return new JobBuilder("personasJob", jobRepository)
 			.incrementer(new RunIdIncrementer())
 			.listener(listener)
-			.start(importCSV2DBStep1)
-			.next(exportDB2CSVStep)
+			.start(copyFilesInDir)
 			.build();
 	}
+	
 }
