6084d1e7e9645d80e5077b0ec823c270b4f05241
o-andres-m <elhinchon@gmail.com>
2023-04-05 08:47:59 +0200

    Fix Actor Entity Cascade.REMOVE with film_actor table

--- demo-web/src/main/java/com/example/domains/entities/Actor.java
+++ demo-web/src/main/java/com/example/domains/entities/Actor.java
@@ -50,7 +50,7 @@ public class Actor extends EntityBase<Actor> implements Serializable {
 	private Timestamp lastUpdate;
 
 	//bi-directional many-to-one association to FilmActor
-	@OneToMany(mappedBy="actor", fetch = FetchType.LAZY)
+	@OneToMany(mappedBy="actor", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
 	private List<FilmActor> filmActors = new ArrayList<>();
 
 	public Actor() {
