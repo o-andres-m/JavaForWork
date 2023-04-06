64560c31aefb0ab9793704f17220b03b64fbe8d1
o-andres-m <elhinchon@gmail.com>
2023-04-06 16:09:29 +0200

    Add ActorResourceTest

---
+++ javaFilms/src/test/java/com/films/application/resources/ActorResourceTest.java
@@ -0,0 +1,252 @@
+package com.films.application.resources;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.films.domains.contracts.services.ActorService;
+import com.films.domains.entities.Film;
+import com.films.domains.entities.Actor;
+import com.films.domains.entities.FilmActor;
+import com.films.domains.entities.dto.ActorDTO;
+import com.films.domains.entities.dto.ActorShort;
+
+import lombok.Value;
+
+@WebMvcTest(ActorResource.class)
+class ActorResourceTest {
+
+	@Autowired
+    private MockMvc mockMvc;
+	
+	@MockBean
+	private ActorService srv;
+
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	
+	@Value
+	static class ActorShortMock implements ActorShort {
+		int actorId;
+		String nombre;
+	}
+
+	@Nested
+	class GetAll{
+		
+		@Test
+		void testGetAll() throws Exception {
+			
+			List<ActorShort> lista = new ArrayList<>(
+			        Arrays.asList(
+			        		new ActorShortMock(1, "Pepito Grillo"),
+			        		new ActorShortMock(2, "Carmelo Coton"),
+			        		new ActorShortMock(3, "Capitan Tan"))
+			        		);
+			
+			when(srv.getByProjection(ActorShort.class)).thenReturn(lista);
+			
+			mockMvc.perform(get("/api/actores/v1").accept(MediaType.APPLICATION_JSON))
+				.andExpectAll(
+						status().isOk(), 
+						content().contentType("application/json"),
+						jsonPath("$.size()").value(3)
+						).andDo(print());
+		}
+		
+		@Test
+		void testGetWithSort() throws Exception {
+			
+			List<ActorShort> listaOrdenadaByName = new ArrayList<>(
+			        Arrays.asList(
+			        		new ActorShortMock(3, "Capitan Tan"),
+	        				new ActorShortMock(2, "Carmelo Coton"),
+			        		new ActorShortMock(1, "Pepito Grillo")
+			        		));
+	
+			
+			when(srv.getByProjection(Sort.by("firstName"), ActorShort.class)).thenReturn(listaOrdenadaByName);
+			
+			mockMvc.perform(get("/api/actores/v1")
+					.param("sort", "firstName")
+					.accept(MediaType.APPLICATION_JSON))
+				.andExpectAll(
+						status().isOk(), 
+						content().contentType("application/json"),
+						jsonPath("$.size()").value(3),
+						jsonPath("$[0].nombre").value("Capitan Tan")
+						).andDo(print());
+		}
+	}
+
+
+	@Test
+	void testGetAllPageable() throws Exception {
+		List<ActorDTO> lista = new ArrayList<>(
+		        Arrays.asList(new ActorDTO(1, "Pepito", "Grillo"),
+		        		new ActorDTO(2, "Carmelo", "Coton"),
+		        		new ActorDTO(3, "Capitan", "Tan")));
+
+		when(srv.getByProjection(PageRequest.of(0, 20), ActorDTO.class))
+			.thenReturn(new PageImpl<>(lista));
+		
+		mockMvc.perform(get("/api/actores/v1").queryParam("page", "0"))
+			.andExpectAll(
+				status().isOk(), 
+				content().contentType("application/json"),
+				jsonPath("$.content.size()").value(3),
+				jsonPath("$.size").value(3)
+				);
+	}
+	
+	@Nested
+	class Pelis{
+		@Test
+		void testGetPelis() throws Exception {
+			var id = 1;
+			var actor = new Actor(id, "Pepito", "Grillo");
+			
+			var actorFilms=new ArrayList<FilmActor>(
+					Arrays.asList(
+							new FilmActor(new Film(),actor),
+							new FilmActor(new Film(),actor)
+							));
+			
+			actor.setFilmActors(actorFilms);
+			
+			when(srv.getOne(actor.getActorId())).thenReturn(Optional.of(actor));
+			
+			mockMvc.perform(get("/api/actores/v1/{id}/pelis", id))
+			.andExpectAll(
+				status().isOk(),
+				jsonPath("$.size()").value(2)
+				)
+		        .andDo(print());
+		}
+		@Test
+		void testGetPelisActorNotFound() throws Exception {
+			
+			int id = 1;
+			var ele = new Actor(id, "Pepito", "Grillo");
+			
+			when(srv.getOne(id)).thenReturn(Optional.empty());
+			
+			mockMvc.perform(get("/api/actores/v1/{id}/pelis", id))
+				.andExpect(status().isNotFound())
+				.andExpect(jsonPath("$.title").value("Not Found"))
+		        .andDo(print());
+		}
+	}
+
+	@Test
+	void testGetOne() throws Exception {
+		int id = 1;
+		var ele = new Actor(id, "Pepito", "Grillo");
+		
+		when(srv.getOne(id)).thenReturn(Optional.of(ele));
+		
+		mockMvc.perform(get("/api/actores/v1/{id}", id))
+			.andExpect(status().isOk())
+	        .andExpect(jsonPath("$.id").value(id))
+	        .andExpect(jsonPath("$.nombre").value(ele.getFirstName()))
+	        .andExpect(jsonPath("$.apellido").value(ele.getLastName()))
+	        .andDo(print());
+	}
+	
+	@Test
+	void testGetOne404() throws Exception {
+		
+		int id = 1;
+		var ele = new Actor(id, "Pepito", "Grillo");
+		
+		when(srv.getOne(id)).thenReturn(Optional.empty());
+		
+		mockMvc.perform(get("/api/actores/v1/{id}", id))
+			.andExpect(status().isNotFound())
+			.andExpect(jsonPath("$.title").value("Not Found"))
+	        .andDo(print());
+	}
+
+	@Test
+	void testCreate() throws Exception {
+		int id = 1;
+		var ele = new Actor(id, "Pepito", "Grillo");
+		
+		when(srv.add(ele)).thenReturn(ele);
+		
+		mockMvc.perform(post("/api/actores/v1")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(ActorDTO.from(ele)))
+			)
+			.andExpect(status().isCreated())
+	        .andExpect(header().string("Location", "http://localhost/api/actores/v1/1"))
+	        .andDo(print());
+	}
+
+	@Test
+	void testUpdate() throws JsonProcessingException, Exception {
+		
+		var actor = new Actor(1, "Pepito", "Grillo");
+
+		when(srv.modify(actor)).thenReturn(actor);
+		
+		mockMvc.perform(put("/api/actores/v1/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(ActorDTO.from(actor)))
+				)
+				.andExpect(status().isNoContent())
+		        .andDo(print());
+	}
+
+	@Test
+	void testDelete() throws JsonProcessingException, Exception {
+		var actor = new Actor(1, "Pepito", "Grillo");
+
+
+		doNothing().when(srv).deleteById(1);
+		
+		mockMvc.perform(delete("/api/actores/v1/1")
+				)
+				.andExpect(status().isNoContent())
+		        .andDo(print());	
+		
+		//El verify "verifica" que se ha llamado al metodo mockeado
+		//Hay que ponerlo luego de donde se "llama" al metodo:
+		verify(srv,times(1)).deleteById(1);
+		
+	}
+
+}
