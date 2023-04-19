d2c9ddd2c03a782cb6f45c8d75875df9df53cffb
o-andres-m <elhinchon@gmail.com>
2023-04-19 09:41:59 +0200

    Fix from Profe

--- demo-react/src/App.js
+++ demo-react/src/App.js
@@ -1,224 +1,135 @@
 import logo from './logo.svg';
 import './App.css';
 
-//Al poner rcc el vscode nos crea un componente y nos importa lo necesario:
+
 import React, { Component } from 'react'
-
-//
 import { Card, Contador } from './componentes';
+import { ErrorBoundary } from './comunes';
 
-//Componente "App"
 export default class App extends Component {
-
-constructor(props){
-  super(props)
-  this.state = {
-    cont : 0
+  constructor(props) {
+    super(props)
+    this.state = {
+        cont: 0,
+        main: 0
+    }
+    this.menu = [
+      { texto: 'inicio', url: '/', componente: <Home />},
+      { texto: 'demos', url: '/demos', componente: <DemosJSX /> },
+      { texto: 'contador', url: '/contador', componente: <Contador init={69} />},
+      { texto: 'ejemplos', url: '/ejemplos', componente: <Ejemplos />},
+    ]
   }
-}
 
   render() {
     return (
       <>
-        {/*PARA PODER VERLOS HAY QUE DESCOMENTARLOS!!!!*/}
-        {/*<Home />*/}
-        {/*<Calculadora />*/}
-        {/*<DemosJSX/>  */}
-
-        {/*El Contador esta en otro archivo, pero cuando lo ponemos, lo importa solo!*/}
+        <Cabecera menu={this.menu} onSelectMenu={indice => this.setState({main: indice})} />
         <main className='container-fluid'>
-          <Card title = "Ejemplo de Componente">
-            <Contador init={10} delta={2} onChange={num => this.setState({cont: num})}/>
-          </Card>
-          <p>El contador: {this.state.cont}</p>
-        </main>    
-    </>
-    )
-  }
-}
-/**
- * Para devolver componentes, deben ser si o si 1 etiqueta, si dentro tiene otras, no pasa nada
- * En este caso tenemos 1 etiqueta DIV que dentro tiene otra
- * Podemos devolver una LISTA de etiquetas sino:
- *     return [
-        <Home />
-        <DemosJSX />
- ]
- En este caso devuelve una LISTA de etiquetas
- Y por ultimo podemos devolver con una etiqueta diamante:
-       <>
-        <Home />
-        <DemosJSX />
-      </>
-Es como el DIV pero sin la palabra Div
- */
-
-
-//Componente DemosJSX
-//Ojo que por defecto va como DEFAULT, pero ya tenemos un defailt
-class Calculadora extends Component {
-  render() {
-    return (
-      <>
-        <main>
-          <h1>Calculadora</h1>
-          <div className="Calculadora">
-            <output className="Resumen" id="txtResumen" />
-            <output className="Pantalla" id="txtPantalla" />
-            <input
-              id="btnIniciar"
-              className="btnOperar"
-              type="button"
-              defaultValue="C"
-            />
-            <input
-              id="btnBorrar"
-              className="btnOperar col-2x2"
-              type="button"
-              defaultValue="↶ BORRAR"
-            />
-            <input className="btnOperar btnCalc" type="button" defaultValue="+" />
-            <input className="btnDigito btnNum" type="button" defaultValue={7} />
-            <input className="btnDigito btnNum" type="button" defaultValue={8} />
-            <input className="btnDigito btnNum" type="button" defaultValue={9} />
-            <input className="btnOperar btnCalc" type="button" defaultValue="-" />
-            <input className="btnDigito btnNum" type="button" defaultValue={4} />
-            <input className="btnDigito btnNum" type="button" defaultValue={5} />
-            <input className="btnDigito btnNum" type="button" defaultValue={6} />
-            <input className="btnOperar btnCalc" type="button" defaultValue="*" />
-            <input
-              className="btnDigito btnNum"
-              type="button"
-              defaultValue={1}
-              data-valor={1}
-            />
-            <input className="btnDigito btnNum" type="button" defaultValue={2} />
-            <input className="btnDigito btnNum" type="button" defaultValue={3} />
-            <input className="btnOperar btnCalc" type="button" defaultValue="/" />
-            <input className="btnDigito" type="button" defaultValue="±" />
-            <input
-              className="btnDigito btnNum"
-              type="button"
-              defaultValue={0}
-              data-valor={0}
-            />
-            <input className="btnDigito" type="button" defaultValue="," />
-            <input className="btnOperar btnCalc" type="button" defaultValue="=" />
-            {/* 
-              <input id="btnPI" class="btnDigito" type="button" value="&#928">
-              <input class="btnOperar btnCalc" type="button" value="^">
-              */}
-          </div>
+          <ErrorBoundary>
+          {this.menu[this.state.main].componente}
+          </ErrorBoundary>
         </main>
+        <Pie />
       </>
     )
   }
 }
 
+function Cabecera(props) {
+  return (
+    <header>
+      <Menu {...props} />
+    </header>
+  );
+}
+
+function Menu({menu, onSelectMenu}) {
+  return (
+    <nav>
+      {menu.map((item, index) => 
+        <button key={index} type='button' onClick={() => onSelectMenu && onSelectMenu(index)}>{item.texto}</button>)
+      }
+    </nav>
+  );
+}
+
+function Pie() {
+  return null;
+}
+
+class Ejemplos extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+          cont: 0,
+      }
+     }
+  
+    render() {
+      return (
+        <>
+          <main className='container-fluid'>
+            <Card tittle="Ejemplo de componente">
+              <Contador init={10} delta={2} 
+                onChange={num => this.setState({cont: num})} />
+            </Card>
+            <p>El contador: {this.state.cont}</p>
+            <input className='btn btn-bg-danger' type='button' value='No tocar' onClick={() => { throw new Error('No tocar')} } />
+          </main>
+        </>
+      )
+    }
+  }
 
 class DemosJSX extends Component {
   render() {
-    let nombre = '<b>mundo</b>';
-    let saluda = <h1>Hola {nombre.toUpperCase() + '!'} - !!</h1>;
-    let estilo = 'App-link';
-    let dim = {width:100, heigth:50};
-    let errorStyle = {color:'white',backgroundColor:'red'};
-    let limpia = true;
-    let falsa = { persona : { nombre: "Tiene nombre!" } };
-    //let persona = 'Yo!';
-    let espacio = <br/>;
-
-    let lista = [
-      {id:1 , nombre:'Nombre 1'},
-      {id:2 , nombre:'Nombre 2'},
-      {id:3 , nombre:'Nombre 3'},
-      {id:4 , nombre:'Nombre 4'}
-  ];
-
-
-
+    let nombre = '<b>mundo</b>'
+    let estilo = 'App-link'
+    let saluda = <h1>Hola {nombre.toUpperCase() + '!'}!!</h1>
+    let dim = { width: 100, height: 50 }
+    let errorStyle = { color: 'white', backgroundColor: 'red' }
+    let limpia = true
+    let falsa
+    let list = [
+      {id: 1,nombre:'Madrid'},
+      {id: 2,nombre:'Barcelona'},
+      {id: 3,nombre:'Sevilla'},
+      {id: 4,nombre:'Valencia'},
+    ]
     return (
       <>
-        {/** imprimimos variables: */}
         {saluda}
-        {espacio}
-        {/** imprimimos booleanos, ojo! Con ternarios: */}
-        {limpia ? 'verdadero' : 'falso'}
-        {espacio}
-
-        {/** imprimimos con ternarios con HTML: */}
-        {limpia ? <b>verdadero</b> : <b>falso</b>}
-        {espacio}
-
-        {/** imprimimos con ternarios con HTML y variables: */}
-        {limpia ? <b>verdadero - {nombre}</b> : <b>falso - {nombre}</b>}
-        {espacio}
-
-        {/** Solo si es TRUE , que devuelva lo de al lado, en este caso LIMPIA */}
+        {limpia ? <b>verdadero</b> : <i>falso {nombre}</i>}
         {limpia && <h2>Limpia</h2>}
-        {espacio}
-
-        {/** doble ?? indica imprimir el valor, a menos que no exista: */}
-        {limpia ?? <b>no existe!</b>}
-        {espacio}
-
-
-        {/** doble ?? indica imprimir el valor, a menos que no exista: */}
-        {falsa ? <b>Existe</b>:<b>no existe!</b>}
-        {espacio}
-
-
-        {/** Verificamos si son nulos, cortocircuitando, caso que validen, imprime el ultimo valor de la expresion: */}
-        {falsa !== null && falsa.persona !== null && falsa.persona.nombre}
-        {espacio}
-
-        {/** Esto mismo que arriba pero mas corto, esto se llama COMPROBACION DE NULOS */}
-        {falsa?.persona?.nombre}
-        {espacio}
-
-        {/** Igual que arriba, pero con un imprimir NO EXISTE en caso de que haya un nulo*/}
+        {/* {falsa ?? <b>no existe</b>} */}
+        {/* {falsa !== null && falsa.persona !== null && falsa.persona.nombre} */}
         {falsa?.persona?.nombre ?? <b>no existe</b>}
-        {espacio}
-
-        <div style={{color:'white',backgroundColor:'red'}}>
-          DemosJSX
-        </div>
-        <h2>Hola {nombre}</h2>
-        <h2 className="App-link">Hola {nombre} con class</h2>
-        <h2 className={estilo}>Hola - - <span dangerouslySetInnerHTML={{__html: nombre}}/> con class estilo en variable</h2>
-        <h2 className={estilo} style={errorStyle}>Hola {nombre} con estilo de variable</h2>
-        <img src={logo} className="App-logo" alt="logo" width={100} hidden={false} />
-        <p>Ahora Logo con desestructurar componentes:</p>
-        <img src={logo} className="App-logo" alt="logo" {...dim} hidden={false} />
-
-        <p>Probamos con coleccion de numeros:</p>
-        <ul>
-        {[1,2,3,4,5].map((item,index) => <li key={index}>Elemento {item}</li>)}
-        </ul>
-
-        <p>Probamos con una lista de objetos:</p>
-        <ul>
+        <div style={{ color: 'white', backgroundColor: 'red' }}>DemosJSX</div>
+        <h2 className={estilo} style={errorStyle} >Hola
+          <span dangerouslySetInnerHTML={{ __html: nombre }} /></h2>
+          <ul>
+            {[1,2,3,4, 3, 2, 1].map((item,index) => <li key={index}>Elemento {item}</li>)}
+          </ul>
           <select>
-            {lista.map(item => <option key={item.id} value={item.id}>{item.nombre}</option>)}
+            {list.map(item => <option key={item.id} value={item.id}>{item.nombre}</option>)}
           </select>
-        </ul>
-
-
+          <img src={logo} className="App-logo" alt="logo" {...dim} hidden={false} />
       </>
     )
   }
 }
 
-
-
 function Home() {
+  let url = process.env.REACT_APP_API_URL
+  
   return (
     // eslint-disable-next-line jsx-quotes
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React!</h1>
-        <h2>{process.env.API_URL}</h2>
+        <h1>Hola mundo</h1>
+        <h2>url: {url}</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -234,5 +145,3 @@ function Home() {
     </div>
   );
 }
-
-//export default App;
--- demo-react/src/componentes.js
+++ demo-react/src/componentes.js
@@ -1,79 +1,66 @@
 import React, { Component } from 'react'
 
-
-function Pantalla(props){
+function Pantalla(props) {
     return <output>{props.valor}</output>
-
 }
 
-function Botonera (onSube, onBaja){
-    return(
+function Botonera({onSube, onBaja}) {
+    return (
         <div>
-            <input type="button" value="-" onClick={() => onBaja && onBaja()}/>
-            {/* <input type="button" value="+" onClick={this.sube}/> */}
-            {onSube && <input type="button" value="+" onClick={onSube}/>}
-            <input type="button" value="Init" onClick={() => this.setState({contador:0})}/>
+            <input type='button' value='-' onClick={ev => onBaja && onBaja(ev)} />
+            {onSube && <input type='button' value='+' onClick={onSube} />}
         </div>
-    );
+    )
 }
 export class Contador extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props)
-
-        this.cont = this.props.init ?? 0;
         this.state = {
             contador: +(this.props.init ?? 0)
         }
-        this.delta = +(this.props.delta ?? 1);
-
+        this.delta = +(this.props.delta ?? 1)
         this.baja = () => {
             this.changeContador(-this.delta)
         }
-        this.sube = this.sube.bind(this)
+        // this.sube = this.sube.bind(this)
     }
 
-    changeContador(value){
-        this.setState(prev => ({contador: prev.contador + value}))
-        //this.setState(prev => ({contador: prev.contador + value}));
+    changeContador(value) {
+        // this.setState(prev => ({contador: prev.contador + value}))
         this.setState(prev => {
-            let result = {contador: prev.contador + value}
-            if(this.props.onChange){
-                this.props.onChange(result.contador);
-            }
+            let result = { contador: prev.contador + value }
+            if (this.props.onChange)
+                this.props.onChange(result.contador)
             return result;
-        });
+        })
     }
 
-    sube(){
+    sube() {
         this.changeContador(this.delta)
     }
-  render() {
-    return (
-      <div>
-        <Pantalla valor={this.state.contador} />
-        <Botonera onBaja={this.baja} onSube={this.sube.bind(this)} />
-        
-{/*     Forma anterior de Botonera:
-         <div>
-            <input type="button" value="-" onClick={this.baja}/>
-            <input type="button" value="+" onClick={this.sube}/>
-            <input type="button" value="Init" onClick={() => this.setState({contador:0})}/>
-        </div>
- */}
-      </div>
-    )
-  }
+
+    render() {
+        return (
+            <div>
+                <Pantalla valor={this.state.contador} />
+                <Botonera onBaja={this.baja} onSube={this.sube.bind(this)} />
+                {/* <div>
+            <input type='button' value='-' onClick={this.baja} />
+            <input type='button' value='+' onClick={this.sube.bind(this)} />
+            <input type='button' value='Init' onClick={() => this.setState({contador: 0})} />
+        </div> */}
+            </div>
+        )
+    }
 }
 
 export class Card extends Component {
-  render() {
-    return (
-
-      <div>
-        <h1>{this.props.title}</h1>
-        {this.props.children}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>{this.props.tittle}</h1>
+                {this.props.children}
+            </div>
+        )
+    }
 }
