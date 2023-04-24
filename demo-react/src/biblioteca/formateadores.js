15631282ebd95332897d71ceb5dc536ebe02f6ed
o-andres-m <elhinchon@gmail.com>
2023-04-24 09:56:13 +0200

    Add Tests to calculadora y formateador + coverage

--- demo-react/src/biblioteca/formateadores.js
+++ demo-react/src/biblioteca/formateadores.js
@@ -1,6 +1,7 @@
 export function titleCase(str) {
-    return str?.toLowerCase()
+    if (typeof(str) !== 'string') return str;
+    return (str?.toString().trim().toLowerCase()
         .split(' ')
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
+        .join(' '));
 }
